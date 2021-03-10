113beaf9df888a681579294aa9de26297c0db854
kuba desrgawy <kuba.hlavacek567@seznam.cz>
2021-03-10 13:06:03 +0100

    cv5

--- labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/xsim_webtalk.tcl
+++ labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Mar  3 22:03:59 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Mar  3 22:10:29 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "c96c5c70-3a3f-4353-a9a6-7f0d492a5f27" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "910b2ebe5ab346718ce31a1027d99b9a" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3550H with Radeon Vega Mobile Gfx  " -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "3" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6380_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3245324922 -regid "" -xml C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key runtime -value "1600 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6432_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1635834982 -regid "" -xml C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kubah/OneDrive/Plocha/DE1/Digital-electronics-1/labs/04-7seg/DE_1_cv4/DE_1_cv4.sim/sim_1/behav/xsim/xsim.dir/tb_top2_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
